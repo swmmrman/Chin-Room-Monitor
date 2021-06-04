3ce2b1bae4b90100bee3e34a9b61523f5bb93b50
Stephan Kern <stephan@swmmrman.com>
2021-06-04 06:57:01 -0600

    Add dummy function to implement sound alarm, Add params to call

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import os
 
 monitor_path = '/dev/ttyUSB0'
 
+def soundAlarm(station, temp):
+    # dummy function
+    print(F"Critical Temp reached on Station {station}: {temp}f")
+
 if not os.path.exists(monitor_path):
     print(F"Device: {monitor_path} not found.")
     sys.exit(1)
@@ -16,6 +20,6 @@ with serial.Serial(monitor_path, 115200) as ser:
         station_number = station.strip("R")
         uptime = count.split(":")[1]
         temp = float(temp.strip('f'))
-        if temp > 70:
-            soundAlarm()
+        if temp > 75:
+            soundAlarm(station_number, temp)
         print(F"Station: {station_number} Status: {temp}f, {humidity} Uptime:{uptime}")
