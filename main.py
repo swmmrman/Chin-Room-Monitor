adffd69ba2dc8e6e6b868ee7e1559bbc8f8c244e
Stephan Kern <stephan@swmmrman.com>
2021-06-04 07:09:49 -0600

    Add seperate high/critical temps, and logic to drive

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import sys
 import os
 
 monitor_path = '/dev/ttyUSB0'
+high_temp = 70
+crit_temp = 73
 
-def soundAlarm(station, temp):
+def soundAlarm(station, temp, **kwargs):
     # dummy function
-    print(F"Critical Temp reached on Station {station}: {temp}f")
+    for key, value in kwargs:
+        alert_type = "High"
+        if key == crit and value:
+            alert_type="Critical"
+            # other crit code here.
+    print(F"{alert_type} temp reached on Station {station}: {temp}f")
+
+
 
 if not os.path.exists(monitor_path):
     print(F"Device: {monitor_path} not found.")
@@ -20,6 +29,8 @@ with serial.Serial(monitor_path, 115200) as ser:
         station_number = station.strip("R")
         uptime = count.split(":")[1]
         temp = float(temp.strip('f'))
-        if temp > 75:
+        if temp > high_temp:
             soundAlarm(station_number, temp)
+        elif temp > crit_temp:
+            soundAlarm(station_number, temp, crit=True)
         print(F"Station: {station_number} Status: {temp}f, {humidity} Uptime:{uptime}")
