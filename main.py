225a8d0ffc773d493132436ce240734e4b7f751a
Stephan Kern <stephan@swmmrman.com>
2021-06-05 09:42:42 -0600

    Tracking stations, and output min/max temp

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import serial
 import sys
 import os
-import libs.stations
+import libs.Stations as Stations
 
 monitor_path = '/dev/ttyUSB0'
 high_temp = 68.5
 crit_temp = 72.5
+
+stations = {}
 
 def soundAlarm(station, temp, **kwargs):
     alert_type = "High"
@@ -30,8 +32,15 @@ with serial.Serial(monitor_path, 115200) as ser:
         station_number = station.strip("R")
         uptime = count.split(":")[1]
         temp = float(temp.strip('f'))
+        humidity = float(humidity.strip("%"))
+        if station_number not in stations:
+            stations[station_number] = Stations.Station(station_number, temp, humidity)
+        else:
+            stations[station_number].update(temp, humidity)
         if temp > high_temp:
             soundAlarm(station_number, temp)
         elif temp > crit_temp:
             soundAlarm(station_number, temp, crit=True)
-        print(F"Station: {station_number} Status: {temp:0.2f}f, {humidity}")
+        min_t = stations[station_number].low_temp
+        max_t = stations[station_number].high_temp
+        print(F"Station: {station_number} Status: {temp:0.2f}f {max_t}f, {min_t}f, {humidity}")
