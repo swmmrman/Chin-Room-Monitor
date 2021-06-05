a1c8f80e8edc050702fb0dc8cd2d12f8f6397ef1
Stephan Kern <stephan@swmmrman.com>
2021-06-05 08:52:30 -0600

    Start of station class to track station status

---
+++ libs/stations.lib.py
@@ -0,0 +1,7 @@
+class station:
+  def __init__(self, number, temp, humidity)
+    self.number = number
+    self.high_temp = temp
+    self.low_temp = temp
+    self.high_humidity = humidity
+    self.low_humidity = humidity
