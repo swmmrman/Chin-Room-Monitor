273e6fa6be4ff623a1982b90841474f3a27ef23e
Stephan Kern <stephan@swmmrman.com>
2021-06-05 08:59:03 -0600

    Formatting

--- libs/stations.py
+++ libs/stations.py
@@ -1,7 +1,8 @@
 class station:
-  def __init__(self, number, temp, humidity):
-    self.number = number
-    self.high_temp = temp
-    self.low_temp = temp
-    self.high_humidity = humidity
-    self.low_humidity = humidity
+    def __init__(self, number, temp, humidity):
+        self.number = number
+        self.high_temp = temp
+        self.low_temp = temp
+        self.high_humidity = humidity
+        self.low_humidity = humidity
+    
