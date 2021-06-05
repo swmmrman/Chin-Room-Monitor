24ab5da112712c64f9f99eb78d13648efe724ccb
Stephan Kern <stephan@swmmrman.com>
2021-06-05 09:59:23 -0600

    Store current temp/humid

--- libs/Stations.py
+++ libs/Stations.py
@@ -3,10 +3,14 @@ class Station:
         self.number = number
         self.high_temp = temp
         self.low_temp = temp
+        self.current_temp = temp
         self.high_humidity = humidity
         self.low_humidity = humidity
+        self.current_humidity = humidity
 
     def update(self, temp, humidity):
+        self.current_humidity = humidity
+        self.current_temp = temp
         if self.high_temp < temp:
             self.high_temp = temp
         elif self.low_temp > temp:
