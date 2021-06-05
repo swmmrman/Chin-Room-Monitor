c0119ae3074a284070aeb7c2cebe40f39d98fde6
Stephan Kern <stephan@swmmrman.com>
2021-06-05 09:13:54 -0600

    Update method

--- libs/stations.py
+++ libs/stations.py
@@ -5,4 +5,13 @@ class station:
         self.low_temp = temp
         self.high_humidity = humidity
         self.low_humidity = humidity
-    
+
+    def update(self, temp, humidity):
+        if self.high_temp < temp:
+            self.high_temp = temp
+        elif self.low_temp > temp:
+            self.low_temp = temp
+        if self.high_humidity < humidity:
+            self.high_humidity = humidity
+        elif self.low_humidity > humidity:
+            self.low_humidity = humidity
