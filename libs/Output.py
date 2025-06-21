ecb1464cf49cfbcbcb131af5f2dba9a6482a0d1d
Stephan Kern <stephan@swmmrman.com>
2025-06-21 03:06:55 -0600

    update output lib

--- libs/Output.py
+++ libs/Output.py
@@ -1,8 +1,16 @@
 import sys
+import os
+
 class Output:
     def __init__(self, target):
         self.target = target
-
+        parts = target.split("/")
+        dir = "/".join(parts[0:-1])
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        f = open(target, "w")
+        f.write("")
+        f.close()
 
     def update_page(self, message):
         f = open(self.target, "w")
