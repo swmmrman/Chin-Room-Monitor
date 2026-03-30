6ffe102eb726fef718eaed0a2981cadcfb6ecf7a
Stephan Kern <stephan@swmmrman.com>
2026-03-29 23:31:21 -0600

    overwrite all nans

--- libs/Stations.py
+++ libs/Stations.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Station:
     def __init__(self, number, temp, humidity):
         self.number = number
@@ -11,13 +14,13 @@ class Station:
     def update(self, temp, humidity):
         self.current_humidity = humidity
         self.current_temp = temp
-        if self.high_temp < temp:
+        if self.high_temp < temp or math.isnan(self.high_temp):
             self.high_temp = temp
-        elif self.low_temp > temp:
+        elif self.low_temp > temp or math.isnan(self.low_temp):
             self.low_temp = temp
-        if self.high_humidity < humidity:
+        if self.high_humidity < humidity or math.isnan(self.high_humidity):
             self.high_humidity = humidity
-        elif self.low_humidity > humidity:
+        elif self.low_humidity > humidity or math.isnan(self.low_humidity):
             self.low_humidity = humidity
 
     def print_station(self):
